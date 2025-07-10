7f10ba6219cc43d8eae33bd8f603370e36b415d6
jociel <jocielalvesdejesus@gmail.com>
2025-07-09 21:58:14 -0300

    feat: commit de inicialização de projeto

---
+++ src/test/java/com/_adjt/food_service_manager/FoodServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com._adjt.food_service_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodServiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
