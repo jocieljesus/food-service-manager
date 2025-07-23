d4a32b0d600a029402829e57b4cac5f38c0f82da
Cauana <cauana.dias@hotmail.com>
2025-07-23 20:34:54 -0300

    feat: crud usuario

--- src/main/java/com/adjt/food_service_manager/repository/UserRepository.java
+++ src/main/java/com/adjt/food_service_manager/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.adjt.food_service_manager.repository;
 
 
-import com.adjt.food_service_manager.model.UserModel;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.adjt.food_service_manager.model.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     Optional<UserModel> findByLogin (String login);
