3ada250cd3b4ab92b35d30b2ddfa736aecfc53f7
Ivan Domingos Moreira <ivandmoreira@gmail.com>
2025-07-30 02:03:58 -0300

    Adição dos DTOs, classe mapper para converter entre DTO e entity, ajuste no UsuarioController para usar DTO

---
+++ src/main/java/com/adjt/food_service_manager/dto/UsuarioResponseDTO.java
@@ -0,0 +1,19 @@
+package com.adjt.food_service_manager.dto;
+
+import com.adjt.food_service_manager.enums.TipoUsuarioEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record UsuarioResponseDTO(
+
+        Long id,
+        String nome,
+        String email,
+        String login,
+        LocalDateTime dtUltimaalteracao,
+        TipoUsuarioEnum tipoUsuarioEnum,
+        EnderecoResponseDTO endereco
+) {}
--- src/main/java/com/adjt/food_service_manager/model/EnderecoModel.java
+++ src/main/java/com/adjt/food_service_manager/model/EnderecoModel.java
@@ -1,20 +1,26 @@
 package com.adjt.food_service_manager.model;
 
+import com.adjt.food_service_manager.dto.EnderecoRequestDTO;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Embeddable
 public class EnderecoModel {
 
-    @NotBlank(message = "Logradouro é obrigatório")
-    @Size(max = 100)
+
     private String logradouro;
 
-    private  int numero;
+    private  String numero;
 
     @NotBlank(message = "Bairro é obrigatório")
     @Size(max = 50)
@@ -31,5 +37,18 @@ public class EnderecoModel {
     @NotBlank(message = "CEP é obrigatório")
     @Pattern(regexp = "\\d{5}-\\d{3}", message = "CEP deve estar no formato 00000-000")
     private String cep;
+
+
+    public EnderecoModel(EnderecoRequestDTO enderecoRequestDTO){
+
+        this.logradouro = enderecoRequestDTO.logradouro();
+        this.numero = enderecoRequestDTO.numero();
+        this.bairro = enderecoRequestDTO.bairro();;
+        this.cidade = enderecoRequestDTO.cidade();
+        this.estado = enderecoRequestDTO.estado();
+        this.cep = enderecoRequestDTO.cep();
+
+    }
+
 
 }
