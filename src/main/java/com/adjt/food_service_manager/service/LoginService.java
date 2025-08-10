4e2365d5e2e8e6b9838c75ec12a2d2a7301c0596
freela7pay <freela@7pay.com.br>
2025-08-10 05:03:05 -0300

    adição de novo endpoint para autenticação e troca de senha alem da atualização da collection

--- src/main/java/com/adjt/food_service_manager/service/LoginService.java
+++ src/main/java/com/adjt/food_service_manager/service/LoginService.java
@@ -2,6 +2,7 @@ package com.adjt.food_service_manager.service;
 
 import com.adjt.food_service_manager.dto.LoginRequestDTO;
 import com.adjt.food_service_manager.dto.LoginResponseDTO;
+import com.adjt.food_service_manager.dto.TrocaSenhaRequestDTO;
 import com.adjt.food_service_manager.model.UsuarioModel;
 import com.adjt.food_service_manager.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +18,12 @@ public class LoginService {
 
     public LoginResponseDTO autenticar(LoginRequestDTO requestDTO){
         try{
-            Optional<UsuarioModel> usuarioModel = usuarioRepository.findByLogin(requestDTO.login());
+            Optional<UsuarioModel> usuarioModel = usuarioRepository.findByLoginAndSenha(requestDTO.login(), requestDTO.senha());
 
             if(usuarioModel.isPresent()){
-                UsuarioModel usuario = usuarioModel.get();
+;
+                return new LoginResponseDTO(true, "Login ok. Bem vindo ao food-service-manager",  usuarioModel.get().getNome());
 
-                if(usuario.getSenha().equals(requestDTO.senha())){
-                    return new LoginResponseDTO(true, "Login ok. Bem vindo ao food-service-manager", usuario.getNome());
-
-                }
             }
             return new LoginResponseDTO(false,"Login ou senha inválidos",null);
 
@@ -33,6 +31,30 @@ public class LoginService {
             e.printStackTrace();
             return new LoginResponseDTO(false,"Erro interno ao processar login", null);
         }
-
     }
+
+
+    public LoginResponseDTO trocarSenha(TrocaSenhaRequestDTO trocaSenhaRequestDTO){
+        try{
+            Optional<UsuarioModel> usuarioModel = usuarioRepository.findByLoginAndSenha(trocaSenhaRequestDTO.login(), trocaSenhaRequestDTO.senha());
+
+            if(usuarioModel.isPresent()){
+
+                UsuarioModel usuariologado = usuarioModel.get();
+                usuariologado.setSenha(trocaSenhaRequestDTO.novaSenha());
+                usuarioRepository.save(usuariologado);
+                return new LoginResponseDTO(true, "Senha Alterada com sucesso",  usuarioModel.get().getNome());
+
+            }
+            return new LoginResponseDTO(false,"Login ou Senha Inválidos",null);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return new LoginResponseDTO(false,"Erro interno ao processar alteração de senha", null);
+        }
+    }
+
+
+
+
 }
